e91597f735c18048c3b7d74eee4a7b0652b6b317
Lakshay Chauhan <70480042+imlakshaychauhan@users.noreply.github.com>
2024-11-02 13:19:31 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (59.78%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.cpp
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -4,10 +4,8 @@ public:
         int n = sentence.size();
         for(int i = 0; i < n; i++) {
             char current = sentence[i];
-            if(i == n - 1) {
-                if(current != sentence[0]) return false;
-                else break;
-            }
+            if(i == n - 1)
+                return (current == sentence[0]);
 
             if(current == ' ' && sentence[i + 1] != sentence[i - 1])
                 return false;
